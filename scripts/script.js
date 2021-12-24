7a24ea5b767bc3725c053fd10c6d2485c91c1b2d
cimris <77905334+cimris@users.noreply.github.com>
2021-12-23 19:48:25 -0700

    added more keylinks

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,17 @@ window.addEventListener("keydown", function (event) {
     case "p":
       window.open(protonmail.href, "_self");
       break;
+    case "g":
+      window.open(gmail.href, "_self");
+      break;
+    case "P":
+      window.open(protondrive.href, "_self");
+      break;
+    case "G":
+      window.open(gdrive.href, "_self");
+      break;
     default:
-      this.alert("Invalid input.");
+      console.log(event.key);
       break;
   }
 });
