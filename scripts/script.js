7ca199efb8c3d2b0a82d94faa82858e5e1c11701
cimris <77905334+cimris@users.noreply.github.com>
2021-12-23 19:15:23 -0700

    added start of switch statement

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 //My Scripts
-window.addEventListener("keydown", function (e) {
-  console.log(e.key);
+window.addEventListener("keydown", function (event) {
+  switch (event.key) {
+    case "e":
+      console.log(event.key);
+      break;
+  }
 });
 //fuck I am tired shit ain't working
