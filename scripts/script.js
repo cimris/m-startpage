fc3d4dd3894777c78bf3bee51432cf6b2f38047a
cimris <77905334+cimris@users.noreply.github.com>
2021-12-23 19:52:40 -0700

    added dev links

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,18 @@ window.addEventListener("keydown", function (event) {
     case "G":
       window.open(gdrive.href, "_self");
       break;
+    case "i":
+      window.open(github.href, "_self");
+      break;
+    case "d":
+      window.open(devdocs.href, "_self");
+      break;
+    case "o":
+      window.open(theodinproject.href, "_self");
+      break;
+    case "f":
+      window.open(freecodecamp.href, "_self");
+      break;
     default:
       console.log(event.key);
       break;
